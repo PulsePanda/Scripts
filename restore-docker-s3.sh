10725cdaba45d832c481fff7ea87af4e8602c094
Austin VanAlstyne <ajv857@gmail.com>
2025-03-16 19:05:41 -0500

    Update restore-docker-s3.sh

--- restore-docker-s3.sh
+++ restore-docker-s3.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# Define variables
+# Define variables 
+# CHANGEME
+# CHANGEME
+# CHANGEME
+# CHANGEME
 BUCKET_NAME="ff-backups"
 BACKUP_DIR="/media/backup-temp"  # Local directory for storing downloaded backup files
-FOLDER_NAME="mediadocker"
-DATE="your-backup-date"  # e.g., "2023-10-05" or any date format matching your backup structure
+FOLDER_NAME="CHANGEME"
+DATE="2025-03-16"  # e.g., "2023-10-05" or any date format matching your backup structure
 
 # Create backup directory if it doesn't exist
-mkdir -p $BACKUP_DIR
+mkdir -p $BACKUP_DIR/
 
 # Function to download backups from S3
 download_from_s3() {
     echo "Downloading backup files from S3..."
 
-    s3cmd sync s3://$BUCKET_NAME/$FOLDER_NAME/$DATE/ $BACKUP_DIR --recursive
+    s3cmd sync s3://$BUCKET_NAME/$FOLDER_NAME/$DATE/ $BACKUP_DIR/ --recursive
 
     if [ $? -eq 0 ]; then
         echo "Backup files successfully downloaded from s3://$BUCKET_NAME/$FOLDER_NAME/$DATE/"
@@ -48,7 +52,7 @@ restore_docker_volumes() {
         docker volume create $VOLUME_NAME
 
         # Create a temporary container to restore the volume's data
-        docker run --rm -v ${VOLUME_NAME}:/volume -v $BACKUP_DIR:/backup ubuntu sh -c "tar xzf /backup/${VOLUME_NAME}.tar.gz -C /volume"
+        docker run --rm -v ${VOLUME_NAME}:/volume -v $BACKUP_DIR:/backup busybox sh -c "tar xzf /backup/${VOLUME_NAME}.tar.gz -C /volume"
         if [ $? -eq 0 ]; then
             echo "Restored volume data in $VOLUME_NAME from $VOLUME_TAR_GZ"
         else
@@ -69,10 +73,22 @@ cleanup_local_files() {
     fi
 }
 
+# List all images
+run_all_images() {
+    IMAGES=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -v "<none>")
+
+    # Run a container from each image
+    for IMAGE in $IMAGES; do
+        echo "Attempting to run container from image: $IMAGE"
+        docker run -d $IMAGE || echo "Failed to run container from image: $IMAGE"
+    done
+}
+
 # Execute restore functions
 download_from_s3
 restore_docker_images
 restore_docker_volumes
 cleanup_local_files
+#run_all_images
 
 echo "Restore process completed."
